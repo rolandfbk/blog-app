85feac4ea936c54801a4caabd7924cb92062f2a4
rolandfbk <rolandfbk@gmail.com>
2022-08-17 16:51:09 +0200

    users and posts views implementation done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,16 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments, :likes)
+    @posts.each do |post|
+      post.update_posts_comments_counter
+      post.update_posts_likes_counter
+    end
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @user = @post.user
+    @comments = @post.comments
+  end
 end
