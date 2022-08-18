f3affedeada6edea95f24105ec9d68835f543f3e
rolandfbk <rolandfbk@gmail.com>
2022-08-18 21:36:17 +0200

    adding all forms done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -27,14 +27,11 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @new_comment = current_user.comments.new(
-      text: comment_params,
-      user_id: current_user.id,
-      post_id: @post.id
-    )
-    @new_comment.post_id = @post.id
-    if @new_comment.save
-      redirect_to "/users/#{@post.user_id}/posts/#{@post.id}"
+    @comment = current_user.comments.new(post_id: @post.id, user_id: current_user.id, text: comment_params)
+    @comment.post_id = @post.id
+    @comment.update_comments_posts_counter
+    if @comment.save
+      redirect_to user_post_path(current_user, @post.id)
     else
       flash.now[:error] = "Error: Comment could not be saved. Please try again."
     end
