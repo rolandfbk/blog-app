caa0d39877a32d457e529a367143fcb98de5dc8d
Roland Ossisa Yuma <rolandfbk@gmail.com>
2022-08-28 10:04:11 +0200

    Revert "Milestone 11: API Endpoints"

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module BlogApp
   class Application < Rails::Application
-    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
@@ -19,12 +18,5 @@ module BlogApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.debug_exception_response_format = :default
-    config.session_store :cookie_store, key: '_interslice_session'
-
-    config.middleware.use ActionDispatch::Flash
-
-    config.middleware.use config.session_store, config.session_options
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,15 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
   # Defines the root path route ("/")
-  root 'users#index'
+  root "users#index"
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index new create show destroy] do
     end
   end
 
-  resources :posts, only: %i[new create update destroy] do
+  resources :posts, only: [:new, :create, :update, :destroy] do
     resources :comments
     resources :likes
-  end
-
-  namespace :api, defaults: { format: :json } do
-    namespace :v1 do
-      post 'users/sign_in' => 'users#login'
-      get 'posts' => 'posts#index'
-      get 'comments' => 'comments#index'
-      post 'comments/create' => 'comments#create'
-    end
   end
 end
